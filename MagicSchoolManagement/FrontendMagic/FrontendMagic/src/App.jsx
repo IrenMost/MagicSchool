31948498346a864959375af7c184c8aa71c8b5ac
Iren Peter <peterirenkat@gmail.com>
2024-10-30 09:57:04 +0100

    style less "csiricsáré"

--- MagicSchoolManagement/FrontendMagic/FrontendMagic/src/App.jsx
+++ MagicSchoolManagement/FrontendMagic/FrontendMagic/src/App.jsx
@@ -102,7 +102,7 @@ function App() {
     return (
         <>
             <div className="app-container">
-                <p>MAJOOM</p>
+               
                {/* <HouseList />*/}
                 {routes}
 
--- MagicSchoolManagement/FrontendMagic/FrontendMagic/src/pages/HouseList.jsx
+++ MagicSchoolManagement/FrontendMagic/FrontendMagic/src/pages/HouseList.jsx
@@ -1,8 +1,9 @@
 ﻿
 // itt lesz az összes adat fetch
-
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import PointUpdater from "../components/PointUpdater"
+import './HouseList.css';
 
 
 
@@ -52,20 +53,33 @@ const HouseList = () => {
     }
     const houseColors = ["red-gold", "yellow-black", "blue-bronze", "green-silver"];
 
-    return <div className="house-container">
-        {houseList && houseList.map((house, index) => 
-        (<div key={house.houseId} className={`house-box ${houseColors[index]}`}>
-           
-            <h1>{house.houseName}</h1>
-            <h2>Current points: {house.points}</h2>
+    return <div >
+        <div className="house-container">
+        
+        {houseList && houseList.map((house, index) => (
+            <div key={house.houseId} className={`house-box ${houseColors[index]}`}>
 
-            <PointUpdater
-                houseId={house.houseId}
-                setUpdatedHouse={setUpdatedHouse }
+                {/* Oval background for house name */}
+                <div className="house-name-oval">
+                    <h1>{house.houseName}</h1>
+                </div>
+
+                <h2>Current points: {house.points}</h2>
+
+                <PointUpdater
+                    houseId={house.houseId}
+                    setUpdatedHouse={setUpdatedHouse}
                 />
-        </div>))}
-
+            </div>
+        ))}
+        </div>
+        <div>
+            <Link to={"/"}>
+                <button className="back">Back Home</button>
+            </Link>
+        </div> 
     </div>
+    
                
 };
 
